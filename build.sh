a5e363597a338de97f019657efaad8f23bf77747
wvviao <126077595+wvviao@users.noreply.github.com>
2025-02-28 16:36:41 +0800

    Remove vlc

--- build.sh
+++ build.sh
@@ -34,7 +34,6 @@ mv /opt{.bak,}
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e '[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc' | tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf5 install -y code
-dnf5 install -y vlc
 echo "::endgroup::"
 
 echo "::group:: ===Cleanup==="
