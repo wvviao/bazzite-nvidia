f9ace8cec8aa8914e5659aa17d95cb4cd2eb7862
wvviao <126077595+wvviao@users.noreply.github.com>
2025-03-15 11:18:33 +0800

    Update

--- build.sh
+++ build.sh
@@ -16,28 +16,40 @@ set -ouex pipefail
 # dnf5 -y copr disable ublue-os/staging
 
 echo "::group:: ===Install packages==="
-# Google-chrome
-mv /opt{,.bak}
-mkdir /opt
-dnf5 install -y --enablerepo="google-chrome" google-chrome-stable
-mv /opt/google/chrome /usr/lib/google-chrome
-ln -sf /usr/lib/google-chrome/google-chrome /usr/bin/google-chrome-stable
-mkdir -p usr/share/icons/hicolor/{16x16/apps,24x24/apps,32x32/apps,48x48/apps,64x64/apps,128x128/apps,256x256/apps}
-for i in "16" "24" "32" "48" "64" "128" "256"; do
-    ln -sf /usr/lib/google-chrome/product_logo_$i.png /usr/share/icons/hicolor/${i}x${i}/apps/google-chrome.png
-done
-rm -rf /etc/cron.daily
-rmdir /opt/{google,}
-mv /opt{.bak,}
+# Sysexts
+# https://github.com/m2Giles/m2os/blob/0f1d38d1b0374d9a2e638cf79229f544f8a12c18/desktop-packages.sh#L91
+mkdir -p /etc/sysupdate.d/
+tee /usr/lib/tmpfiles.d/my-bazzite-nvidia-sysext.conf <<EOF
+d /var/lib/extensions/ 0755 root root - -
+d /var/lib/extensions.d/ 0755 root root - -
+EOF
 
-# Vscode
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e '[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc' | tee /etc/yum.repos.d/vscode.repo > /dev/null
-dnf5 install -y code
+SYSEXTS=(
+    google-chrome
+    vscode
+)
+
+for SYSEXT in "${SYSEXTS[@]}"; do
+    tee /etc/sysupdate.d/"$SYSEXT".conf <<EOF
+[Transfer]
+Verify=false
+
+[Source]
+Type=url-file
+Path=https://github.com/travier/fedora-sysexts/releases/download/fedora-kinoite-41/
+MatchPattern=$SYSEXT-@v-%a.raw
+
+[Target]
+InstancesMax=2
+Type=regular-file
+Path=/var/lib/extensions.d/
+MatchPattern=$SYSEXT-@v-%a.raw
+CurrentSymlink=/var/lib/extensions/$SYSEXT.raw
+EOF
+done
 
 copr_repos=(
     zeno/scrcpy
-    the4runner/firefox-dev
 )
 
 for repo in "${copr_repos[@]}"; do
@@ -47,7 +59,6 @@ done
 # Layered Applications
 LAYERED_PACKAGES=(
     scrcpy
-    firefox-dev
 )
 
 dnf5 install --setopt=install_weak_deps=False -y "${LAYERED_PACKAGES[@]}"
